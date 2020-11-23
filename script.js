d07e15f4ec1e5343bf5fe896ff98656fcf763e15
alaksandar_km <ebenzomarket@gmail.com>
2020-11-23 15:58:22 +0100

    homework_4

--- script.js
+++ script.js
@@ -70,4 +70,154 @@ console.log(userArrUnderage);
 
 
 
-                        
+//                      ДЗ_JS_4
+
+// - Напишите функцию hello1(), которая при вызове будет 
+// возвращать строку «Привет, JavaScript!».
+
+function hello1(name) {
+    return `Привет, ${name}`;
+}
+alert (hello1('JavaScript!'));
+
+
+
+// - Напишите функцию hello2(), которая при вызове будет 
+// принимать переменную name (например, «Василий») 
+
+function hello2(name) {
+    alert (`Привет, ${name}`);
+}
+hello2('Василий');
+
+
+
+// - Напишите функцию mul(n,m), которая принимает два аргумента и 
+// возвращает произведение этих аргументов. Проверьте ее работу.
+
+function mul(n = +prompt('Введи число', ''), m = +prompt('Введи число', '')) {
+    let result = n * m;
+    if ( (typeof (n || m) === "number") && !isNaN(n || m) && ((n || m) !== undefined)) {
+        return result;
+    }
+}
+alert( mul() );
+
+
+
+// - Создайте функцию repeat(str, n), которая возвращает строку, 
+// состоящую из n повторений строки str.
+// 	  n — по умолчанию 2, str — пустая строка.
+
+function repeat(str = "", n = 2) {
+    var result = "";
+    for (var i = 0; i < n; i++) {
+        if (typeof n === "number" && !isNaN(n)) {
+            result += str;
+        }
+    }
+    return result;
+}
+console.log(repeat ('x', 100));
+// var res27 = repeat ('x', 100000000); // загрузка 100 MB - 27 сек.
+// console.log(res);
+
+// // загрузка 100 MB - 6 сек.:
+// function repeat(str ='', n = 2) { 
+//     var result = '';
+//     for (;;) { 
+//         if (n & 1) result += str; 
+//         n >>= 1;
+//         if (n) str += str;
+//         else break;
+//     }
+//     return result;
+// }
+// var res6 = repeat ('x', 100000000);
+// console.log(res);
+
+
+
+// - Создайте функцию rgb(a, b, c), которая будет принимать три числовых 
+// аргумента и возвращать строку вида «rgb(23,100,134)». Если аргументы 
+// не заданы, считать их равными нулю. Не проверять переменные на тип данных.
+
+function rgb(
+    a = prompt('Введи число от 0 до 255', '0'), 
+    b = prompt('Введи число от 0 до 255', '0'), 
+    c = prompt('Введи число от 0 до 255', '0')
+) {
+    if( ( (a && b && c) > 0 ) && ( (a && b && c) <= 255 ) ) {
+    return `rgb(${a ? a : 0}, ${b ? b : 0}, ${c ? c : 0})`;
+    } 
+}
+alert( rgb() );
+
+
+
+// - Создайте функцию avg() , которая будет находить среднее
+// арифмитическое значение своих аргументов.
+
+function avg() {
+    var result;
+    var leng = 0;
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        if(typeof arguments[i] === "number" && !isNaN(arguments[i])) {
+            leng++;
+            sum += arguments[i];
+        }
+    }
+    result = sum / leng;
+    return result;
+}
+
+
+
+// - * Напишите функцию words(n), которая в зависимости от 
+// переданного в нее целочисленного аргумента n, будет выводить слово 
+// «товар» в нужной форме («12 товаров», но «22 товара»). По умолчанию 
+// аргумент n должен иметь значение 0.
+
+function words(n = prompt('Сколько товаров?', '0')) {
+    let rest10 = n % 10;
+    let rest100 = n % 100;
+
+    if (rest10 === 1 && rest100 !== 11) {
+        alert (n + ' товар');
+    } else if ( (rest10 > 1 && rest10 < 5) && (!(rest100 > 11 && rest100 < 15)) ) {
+        alert (n + ' товара');
+    } else {
+        alert (n + ' товаров')
+    }
+}
+words();
+
+
+
+// -Задание со звездочкой. Пишем функцию решения 
+// квадратного уравнения через дискриминант:
+
+// 		ax² + bx + c = 0 (общий вид уравнения)
+
+// 		D = b2 – 4ac
+// 		D > 0
+// 		x1 = (-b + √D ) / 2a
+// 		x2 = (-b – √D ) / 2a
+// 		D = 0
+// 		x = -b / 2a
+//	    D < 0 - прекращаем решать уравнение;
+
+function disc(a, b, c) {
+    var d = Math.pow(b, 2) - 4 * a * c; // Math.pow(b, 2) -- b ^ 2 or b ** 2 ???
+    if( (d > 0) ) {
+        var x1 = (-b + Math.sqrt(d)) / (2 * a);
+        var x2 = (-b - Math.sqrt(d)) / (2 * a);
+        return [x1, x2];
+    } else if (d === 0) {
+        x3 = -b / 2 * a;
+        return [x3];
+    } else {
+        return;
+    }    
+}
