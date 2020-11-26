f11649a3a95618c13e8d0f85b9aae2e9225e6829
alaksandar_km <ebenzomarket@gmail.com>
2020-11-27 00:34:56 +0100

    homework_7.2

--- homework_7/script.js
+++ homework_7/script.js
@@ -6,6 +6,9 @@
 
 var arr = [1, 2, 3, 4, 4, 5, 2, 9, 4];
 
+// let arrUnique = [...new Set(arr)];
+// console.log(arrUnique);  // or:
+
 function unique (array) {
     var result = [];
     for (let num of array) {
@@ -21,7 +24,7 @@ console.log(arrUnique);
 
 
 
-// - Дан массив пользователей. var users = [
+// 2. - Дан массив пользователей. var users = [
 //  						  {id: 1, name: "Вася", age: 19},
 //   						  {id: 2, name: "Петя", age: 18},
 //   						  {id: 3, name: "Маша", age: 17},
@@ -35,18 +38,35 @@ console.log(arrUnique);
 // 					    	  {id: 11, name: "Павел", age: 16},
 // 						      {id: 12, name: "Денис", age: 20}
 // 						       ];
-// На основании этого массива сформировать новы массив usersAfterYear где все пользователи 
+// На основании этого массива сформировать новый массив usersAfterYear где все пользователи 
 // повзрсолели на год и являются совершенолетними (повзрослевших на год, но оставшимися
 // все еще несовершенолетних пользователей в новый массив usersAfterYear не включать). 
 // Сам массив users должен остаться неизменным. Использовать метод map.
 
-
+var users = [
+     		{id: 1, name: "Вася", age: 19},
+      		{id: 2, name: "Петя", age: 18},
+      		{id: 3, name: "Маша", age: 17},
+      		{id: 4, name: "Света", age: 18},
+      		{id: 5, name: "Наташа", age: 17},
+     		{id: 6, name: "Женя", age: 12},
+    	    {id: 7, name: "Коля", age: 30},
+    		{id: 8, name: "Настя", age: 16},
+    		{id: 9, name: "Антон", age: 19},
+    		{id: 10, name: "Иван", age: 20},
+    		{id: 11, name: "Павел", age: 16},
+    		{id: 12, name: "Денис", age: 20}
+        ];
+        
+var usersAfterYear = users.map(user => (user.age +=1, user));
+usersAfterYear = usersAfterYear.filter(user => user.age >= 18);
+console.log(usersAfterYear);
 
 
 // - На основании массива usersAfterYear, полученного на основании предыдущего задания создать 
 // новый массив usersAfterYearUniqueAge, в котором нет пользователей с одинаковым возрастом. 
 // Если у вас есть два пользователя ({id: 2, name: "Петя", age: 19} и {id: 4, name: "Света", age: 19}, 
-// то вы должны оставить только Петю или Свету одного а не их двоих). 
+// то вы должны оставить только Петю или Свету одного, а не их двоих). 
 // (Это задание похоже на первое задания про массив arr и arrUnique)
 
 
