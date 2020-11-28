c4f1920b3a6231fad7fec0418b198c32ef136352
alaksandar_km <ebenzomarket@gmail.com>
2020-11-28 17:05:15 +0100

    homework_7 without 7.3

--- homework_7/script.js
+++ homework_7/script.js
@@ -7,7 +7,8 @@
 var arr = [1, 2, 3, 4, 4, 5, 2, 9, 4];
 
 // let arrUnique = [...new Set(arr)];
-// console.log(arrUnique);  // or:
+// console.log(arrUnique);  
+//                                     //or:
 
 function unique (array) {
     var result = [];
@@ -21,23 +22,28 @@ function unique (array) {
 
 var arrUnique = unique(arr);
 console.log(arrUnique);
+//                          or:
+
+var arr = [1, 2, 3, 4, 4, 5, 2, 9, 4];
+var arrUnique = arr.reduce((newArr, item) => {
+    console.log(newArr, newArr.indexOf(item));
+    if (newArr.indexOf(item) == -1) {
+        newArr.push(item);
+        return newArr;
+    } else {
+        return newArr;
+    }
+}, [])
+console.log(arrUnique);
 
 
 
-// 2. - Дан массив пользователей. var users = [
-//  						  {id: 1, name: "Вася", age: 19},
-//   						  {id: 2, name: "Петя", age: 18},
-//   						  {id: 3, name: "Маша", age: 17},
-//   						  {id: 4, name: "Света", age: 18},
-//   						  {id: 5, name: "Наташа", age: 17},
-//  						  {id: 6, name: "Женя", age: 12},
-// 	    					  {id: 7, name: "Коля", age: 30},
-// 		    				  {id: 8, name: "Настя", age: 16},
-// 			    			  {id: 9, name: "Антон", age: 19},
-// 				    		  {id: 10, name: "Иван", age: 20},
-// 					    	  {id: 11, name: "Павел", age: 16},
-// 						      {id: 12, name: "Денис", age: 20}
-// 						       ];
+// 2. - Дан массив пользователей. var users = 
+// [{id: 1, name: "Вася", age: 19}, {id: 2, name: "Петя", age: 18}, {id: 3, name: "Маша", age: 17},
+// {id: 4, name: "Света", age: 18},n{id: 5, name: "Наташа", age: 17}, {id: 6, name: "Женя", age: 12},
+// {id: 7, name: "Коля", age: 30}, {id: 8, name: "Настя", age: 16}, {id: 9, name: "Антон", age: 19},
+// {id: 10, name: "Иван", age: 20}, {id: 11, name: "Павел", age: 16}, {id: 12, name: "Денис", age: 20}];
+
 // На основании этого массива сформировать новый массив usersAfterYear где все пользователи 
 // повзрсолели на год и являются совершенолетними (повзрослевших на год, но оставшимися
 // все еще несовершенолетних пользователей в новый массив usersAfterYear не включать). 
@@ -63,7 +69,8 @@ usersAfterYear = usersAfterYear.filter(user => user.age >= 18);
 console.log(usersAfterYear);
 
 
-// - На основании массива usersAfterYear, полученного на основании предыдущего задания создать 
+
+// 3. - На основании массива usersAfterYear, полученного на основании предыдущего задания создать 
 // новый массив usersAfterYearUniqueAge, в котором нет пользователей с одинаковым возрастом. 
 // Если у вас есть два пользователя ({id: 2, name: "Петя", age: 19} и {id: 4, name: "Света", age: 19}, 
 // то вы должны оставить только Петю или Свету одного, а не их двоих). 
@@ -80,9 +87,9 @@ console.log(usersAfterYear);
 // 					car: "subaru",
 // 					telephone: "samsung" 
 // 				}
-// Необходимо сделать так чтобы все значения полей объекта users начинались с заглавной буквы 
+// Необходимо сделать так, чтобы все значения полей объекта users начинались с заглавной буквы 
 // (например не "pavel" a "Pavel"). Для решения этой задачи рекомендую использовать 
-// методы Object.keys или Object.values и методы строк как UpperCase. 
+// методы Object.keys или Object.values и методы строк, как UpperCase. 
 
 var users = {
     					first_name: "pavel",
@@ -91,18 +98,42 @@ var users = {
     					city: "minsk",
     					car: "subaru",
     					telephone: "samsung" 
-    				}
+                    }
+                    
+var userKeyArr = Object.keys(users);
+userKeyArr.forEach(key => { 
+    users[key] = users[key][0].toUpperCase() + users[key].slice(1);
+});
+console.log(users);                  
 
 // var userKeyArr = Object.keys(users);
-// var userKeyStr = userKeyArr.join(", ");
+// var userValueArr = Object.values(users);
 
-var userValueArr = Object.values(user);
-var userValueStr = userValueArr.join(", ");
-console.log(userValueArr);
+// function getFirstLettersUpperCase (x) {
+//     for(var i = 0; i < x.length; i++) {
+//         var result = "";
+//         var input = x[i];
+//         var firstLetter = input.slice(0,1);
+//         var capitalized = input.replace(firstLetter, firstLetter.toUpperCase());
+//         result = capitalized;
+//         console.log(result);
+//     }
+//     return result;
+// }
+// userValueArrNew = getFirstLettersUpperCase (userValueArr); // ???
+// console.log(userValueArrNew);  
+
+// for(var i = 0; i < userValueArr.length; i++) {
+//     var input = userValueArr[i];
+//     var firstLetter = input.slice(0,1);
+//     var capitalized = input.replace(firstLetter, firstLetter.toUpperCase());
+//     var result = capitalized;
+// console.log(result);
+// }
 
 
 
-// - Посчитать первые 8 чисел Фибоначчи использую рекурсию. Например: fibonachi(n); 
+// 5. - Посчитать первые 8 чисел Фибоначчи использую рекурсию. Например: fibonachi(n); 
 // (При вызове функции положите n = 8); 
 
 var fibonachi = (n) => {
