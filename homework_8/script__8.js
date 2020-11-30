22eb5ba437e057a9f4a07944a7983b61f5bfc610
alaksandar_km <ebenzomarket@gmail.com>
2020-11-30 03:02:12 +0100

    homework_8.2 Destructuring assignment (array)

--- homework_8/script__8.js
+++ homework_8/script__8.js
@@ -6,11 +6,15 @@
 
 
 
-// 2. - Дан массив let arr = [1, "str", 24, {name: "Ivan"}, 89, undeifined, "10"]. 
+// 2. - Дан массив let arr = [1, "str", 24, {name: "Ivan"}, 89, undefined, "10"]. 
 // Используя оператор расширения и соответсвие порядка следования элементов запишите первые 
 // два элемента массива в переменные firstElement, secondElement, число 89 (четвертый элемент 
 // массива) в переменную fifthElement. Остальные элементы после 89 запишите в массив otherElements;
 
+let arr = [1, "str", 24, {name: "Ivan"}, 89, undefined, "10"];
+
+[firstElement, secondElement, , , fifthElement, ...otherElements] = arr;
+console.log(firstElement, secondElement, fifthElement, otherElements);
 
 
 
