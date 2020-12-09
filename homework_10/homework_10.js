176642fb960486ed783eb23f093dd2295b22cfb7
alaksandar_km <ebenzomarket@gmail.com>
2020-12-10 00:31:11 +0100

    homework_10.2

--- homework_10/homework_10.js
+++ homework_10/homework_10.js
@@ -5,6 +5,23 @@
 // 		// Ваш код тут
 // 		// → [1, 2, 3, 4, 5, 6]	
 
+// var arrays = [[1, 2, 3], [4, 5], [6]];
+// // 1)
+// let arr = arrays.reduce(function(acc, el, i) {
+//     console.log(acc, el, i);
+//     acc = acc.concat(arrays[i]);
+//     return acc;
+// }, []);
+// // 2)
+// arr = arrays.reduce((acc, el, i) => acc.concat(el));
+// // 3)
+// arr = arrays.reduce((acc, el, i) => [...acc, ...el]);
+// // 4)
+// arr = arrays.reduce((acc, el) => {
+//     console.log(acc, [...el]);
+//     return [...acc, ...el];
+// }, []);
+
 
 
 //     - Создайте функцию-конструктор, которая позволяет создавать объект 
@@ -12,8 +29,18 @@
 // С помощью данной функции создайте два объекта пользователей (два пользователя), 
 // имя, фамилию, возраст, город проживания задайте сами. 
 
+function User(first_name, second_name, age, city) {
+    this.first_name = first_name;
+    this.second_name = second_name; 
+    this.age = age;
+    this.city = city;
+};
+
+let writer = new User("Kurt", "Vonnegut", 84, "Indianapolis");
+let character = new User("Billy", "Pilgrim", 54, "Ilium");
 
 
 //     - Создайте функцию, которая позовляет вывести полную информацию о каждом из двух 
 // пользователей, созданных в предыдущем задании. Функция должна выполнять вывод информации 
 // и не принимать аргументов. По сути нужно в одну функцию привязать два объекта. 
+// (& через prototype)
