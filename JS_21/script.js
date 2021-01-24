8cd106671e20e61ac679a8c573207fd966b8107b
alaksandar_km <ebenzomarket@gmail.com>
2021-01-24 23:42:31 +0100

    cleanRooms

--- JS_21/script.js
+++ JS_21/script.js
@@ -11,26 +11,47 @@
 // Для обработки неуспешной уборки в then используйте console.log(err). 
 // Где err это аргумент reject. Текст ошибки придумайте сами.
 
-let dirty = +prompt('Определите уровень загрязнения от 0 до  10:', "");
+// // let dirtyLevel = +prompt('Определите уровень загрязнения от 0 до  10:', "");
 
-const cleanRoom = dirtyLevel => {
-    return new Promise((resolve, reject) => {
-        
-        if( dirtyLevel <= 10 && dirtyLevel >= 0 &&
-            typeof dirtyLevel === "number" && !isNaN(dirtyLevel) ) {
+// // function cleanRoom() {
+// //     return new Promise((resolve, reject) => {
 
-            setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000)
+// //         if( dirtyLevel <= 10 && dirtyLevel >= 0 &&
+// //             typeof dirtyLevel === "number" && !isNaN(dirtyLevel) ) {
 
-        } else {
-            setTimeout(() => reject('Ошибка! Уборка не будет выполнена'), dirtyLevel * 1000)
-        }
-    })
-}
+// //             setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000)
 
-cleanRoom(dirty).then(
-    suc => console.log(`Уборка проведена успешно за ${suc} сек.`),
-    err => console.log(err)
-)
+// //         } else {
+// //             setTimeout(() => reject('Ошибка! Уборка не будет выполнена'), dirtyLevel * 1000)
+// //         }
+// //     })
+// // }
+
+// // cleanRoom(dirtyLevel).then(
+// //     suc => console.log(`Уборка проведена успешно за ${suc} сек.`),
+// //     err => console.log(err)
+// // )
+
+// or:
+// let dirtyLevel = +prompt('Определите уровень загрязнения от 0 до  10:', "");
+
+// function cleanRoom() {
+//     let cleanTime = dirtyLevel * 1000;
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 if (dirtyLevel <= 10 && dirtyLevel >= 0 && !isNaN(dirtyLevel)) {
+//                     resolve(cleanTime);
+//                 } else {
+//                     reject('Ошибка! Уборка не будет выполнена');
+//                 }
+//             }, cleanTime)
+//         })
+// }
+
+// cleanRoom(dirtyLevel).then(
+//     success => console.log(`Уборка проведена успешно за ${success / 1000} сек.`),
+//     err => console.log(err)
+// )
 
 // - Задание со звездочкой. Расширьте предыдущее задание так, чтобы можно было осуществить 
 // последовательную уборку трех комнат. Для этого создайте цепочку промисов. Только после того,
@@ -39,8 +60,32 @@ cleanRoom(dirty).then(
 // cleanRoom(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) { ... } должна принимать три аргемента, 
 // каждый из кторых описывает уровень загрязнения каждой комнаты.
 
+function cleanRoom(dirtyLevel) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (dirtyLevel <= 10 && dirtyLevel >= 0 && !isNaN(dirtyLevel)) {
+                resolve(dirtyLevel);
+            } else {
+                reject('Ошибка! Уборка не будет выполнена');
+            }
+        }, dirtyLevel * 1000)
+    })
+}
 
+function cleanRooms(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
+    return cleanRoom(dirtyLevel_1)
+        .then(result => {
+            console.log(`Уборка проведена успешно за ${result} сек.`)
+            return cleanRoom(dirtyLevel_2)
+        }).then(result => {
+            console.log(`Уборка проведена успешно за ${result} сек.`)
+            return cleanRoom(dirtyLevel_3)
+        }).then(result => {
+            console.log(`Уборка проведена успешно за ${result} сек.`)
+        }).catch(err => console.log(err));
+}
 
+cleanRooms(3, 2, 1);
 
 // - Задача на повторение массивов. Дан массив строк (например: 
 // ["str", "str1", "str2", "str", "str2", "str", "str3", "str1", "str4", "str4"]).
@@ -54,3 +99,11 @@ cleanRoom(dirty).then(
 // 			str3: 1,
 // 		}	
 // Обратите внимание, что информация должна быть отсортирована по убыванию повторений.
+
+// function(arr) {
+//     Object.defineProperties(arr)
+// }
+// let promise = new Promise(arr) {
+
+// }
+// let arr = ["str", "str1", "str2", "str", "str2", "str", "str3", "str1", "str4", "str4"];
